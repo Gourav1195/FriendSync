b3ad3689c074f33fd0e87de4b994c353918cc82e
Gourav Modi <gouravmodi1195@gmail.com>
2023-12-05 19:51:17 +0530

    Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-// import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './global.css';
+import { Home } from './_root/pages';
+
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/forms/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
 export const App = () => {
   return (
-    <>
-<h1 className="text-3xl font-bold ">
-      Hello FriendSync!
-     </h1>
-  </>
+  <main className='flex h-screen'>
+  <Routes>
+  {/* public routes can see without sign in */}
+    <Route element={<AuthLayout />}>
+      <Route  path="sign-in" element={<SigninForm />} />
+      <Route path='/sign-up' element={<SignupForm />}  />
+    </Route>
+
+  {/* private route */}
+  <Route element={<RootLayout />} >
+  <Route index element={<Home />} />
+  </ Route>
+  </Routes>
+  </main>
   )
 }
  
