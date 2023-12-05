964bdfbdbbc14d78820df7abd2fffe1bb5641c25
Gourav Modi <gouravmodi1195@gmail.com>
2023-12-05 11:56:08 +0530

    tailwind css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+// import React from 'react';
 import './global.css';
 export const App = () => {
   return (
-<h1 className="text-3xl font-bold underline">
+    <>
+<h1 className="text-3xl font-bold ">
       Hello FriendSync!
-    </h1>
+     </h1>
+  </>
   )
 }
+ 
+//Tailwind allows to create custom style more quickly
