2deda2f6a40e2d5e23604dc51f912ebfd12eb633
Gourav Modi <gouravmodi1195@gmail.com>
2023-12-06 23:06:36 +0530

    Assets added

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import {Outlet, Navigation} from 'react-router-dom';
+import {Outlet, Navigate} from 'react-router-dom';
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
+// item center: Vertically center; justify-center: Horizontally center; flex-col: one below another; py-10: padding on top and bottom
   return (
-    <div>AuthLayout</div>
-  )
+    <>
+      {isAuthenticated ? (<Navigate to='/' />
+      ):(<>
+      <section className='flex flex-1 justify-center items-center flex-col py-10'>
+        <Outlet/>
+        {/* Outlet will show the page which is there, i.e. signup or signin page */}
+      </section>
+      
+      </>)
+}
+    </>
+    )
 }
 
 export default AuthLayout
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -5,10 +5,10 @@ import { Button } from "@/components/ui/button"
 
 const SignupForm = () => {
   return (
-    <div>SignupForm
+    <>
             <Button>Click me</Button>
 
-    </div>
+    </>
   )
 }
 
