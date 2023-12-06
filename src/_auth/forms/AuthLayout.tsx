7ab93071b80db537c45d384b571a2f788d142eec
Gourav Modi <gouravmodi1195@gmail.com>
2023-12-07 02:51:44 +0530

    singup update

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -3,7 +3,7 @@ import {Outlet, Navigate} from 'react-router-dom';
 
 const AuthLayout = () => {
   const isAuthenticated = false;
-// item center: Vertically center; justify-center: Horizontally center; flex-col: one below another; py-10: padding on top and bottom
+// item center: Vertically center; justify-center: Horizontally center; flex-col: one below another; py-10: padding on top and bottom; hidden; xl:block: show on xl devices; h-screen : full height; w-1/2: half;
   return (
     <>
       {isAuthenticated ? (<Navigate to='/' />
@@ -12,7 +12,8 @@ const AuthLayout = () => {
         <Outlet/>
         {/* Outlet will show the page which is there, i.e. signup or signin page */}
       </section>
-      
+        <img src='/assets/images/side-img.svg' alt="logo" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
+
       </>)
 }
     </>
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+const onSubmit = () => {
 
+}
 
 const SignupForm = () => {
+  
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
   return (
     <>
-            <Button>Click me</Button>
-
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
     </>
   )
 }
