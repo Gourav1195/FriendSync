b4c406dfe138281d33744644e869716432f9520b
Gourav Modi <gouravmodi1195@gmail.com>
2024-01-04 00:28:14 +0530

    api updated

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID } from "appwrite";
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user:INewUser) {
     try {
@@ -11,13 +11,41 @@ export async function createUserAccount(user:INewUser) {
             user.password,
             user.name
         );
-
         // create user on Database
         if(!newAccount) throw Error;
+        const avatarUrl = avatars.getInitials(user.name);
 
-        return newAccount;
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            email: newAccount.email,
+            name: newAccount.name,
+            username: user.username, // we are not passing it in newAccount
+            imageUrl: avatarUrl,
+        })
+
+        return newUser;
     } catch (error) {
         console.log(error);
         return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    //accept values and destructured below
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+}){
+    try{
+        const newUser = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        ID.unique(),
+        user,
+    )}
+    catch (error){
+        console.log(error);
     }
 }
