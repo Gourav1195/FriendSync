b42814851ddfa411cfc7ce3f29d2dbe18e970ab5
Gourav Modi <gouravmodi1195@gmail.com>
2024-02-16 00:55:09 +0530

    updated

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,27 +1,21 @@
-import {Client, Account, Databases, Storage, Avatars} from 'appwrite'
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-    url: import.meta.env.VITE_APPWRITE_URL,
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,    
-    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-    SavesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
-
-}
-
-//to use declared elements of appwrite  (Client, Account, Database, Storage, Avatar)
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+};
 
 export const client = new Client();
-client.setProject(appwriteConfig.projectId);
-client.setEndpoint(appwriteConfig.url);
-export const account = new Account(client);
-//We are using "Account" for AUTH functionality in appwrite
 
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
+
+export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 export const avatars = new Avatars(client);
-
-
-
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App'; 
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthContext';
+import { QuerryProvider } from './lib/react-query/QuerryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-    <App />
+        <QuerryProvider>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </QuerryProvider>
     </BrowserRouter>
 )
 
